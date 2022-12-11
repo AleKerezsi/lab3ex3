658cb7fe007caaa8bdadfc4f1e61940a9da914dd
alexandra.kerezsi.18@gmail.com <alexandra.kerezsi.18@gmail.com>
2022-12-11 23:42:58 +0200

    Add project files.

---
+++ lab3ex3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace lab3ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Ex 3 Scrieti o functie care va determina daca un numar este sau nu patrat perfect. Apelati-o si afisati-i rezultatul */
+
+            Console.WriteLine("Introduceti n = ");
+            int n = int.Parse(Console.ReadLine());
+
+            bool patratPerfect = VerificaPatratPerfect(n);
+
+            if (patratPerfect == true) Console.WriteLine("Numarul " + n + " este patrat perfect");
+            else Console.WriteLine("Numarul " + n + " nu este patrat perfect");
+
+        }
+
+        public static bool VerificaPatratPerfect(int x) 
+        {
+            for (int i = 1; i <= x; i++) 
+                if (x == i * i)
+                    return true;
+            return false;
+        }
+    }
+}
